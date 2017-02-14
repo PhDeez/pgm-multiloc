dc8782394b144673e02fe2b7c9c74e19c17ffcb1
LORD-SS <slgphantom@gmail.com>
2017-02-14 21:20:12 +0800

    Update config.ts

--- src/scripts/config.ts
+++ src/scripts/config.ts
@@ -1,11 +1,11 @@
 export class config {
-  public static googleMapsKey: string = 'AIzaSyD8WtOa0Mw_lpppdbXQrORnabd9LK3bri8';
-  public static latitude: number = 38.624691;
-  public static longitude: number = -90.184776;
+  public static googleMapsKey: string = 'AIzaSyBAGQVYKsxZyr76unpfQQmAuUIRUL7pv30';
+  public static latitude: number = 22.357618;
+  public static longitude: number = 113.980928;
   public static zoom: number = 12;
   public static leaps: number = 4;
   public static steps: number = 10;
-  public static maxSteps: number = 25;
+  public static maxSteps: number = 100;
 
   public static os: string = 'windows';
   public static workers: number = 4;
